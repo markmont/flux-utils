d04890e0622105ffb96277ba6898aa898c87c7e6
Mark Montague <markmont@umich.edu>
2015-03-16 12:21:27 -0400

    add overrides to allow certain users to use LSA public accounts even if they have access to other accounts

---
+++ lsa_flux_check_config.py
@@ -0,0 +1,9 @@
+
+# For the following accounts, allow the specified users to use
+# the LSA public allocations even if they are on the userlist for
+# these accounts.
+
+ignore_accounts = {
+     'support_flux': [ 'markmont', 'mrchampe', 'cja', 'nbrack', 'smeyer' ]
+}
+
